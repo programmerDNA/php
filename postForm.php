36a51dcd6de087957a65b964695f96b81bd6bfd3
programmerDNA <programmerdna@gmail.com>
2016-01-15 13:47:49 +0900

    #6 POST 작동 연습 및 쿠키 연습

--- postForm.php
+++ postForm.php
@@ -1,12 +1,46 @@
-<form method="post" action="postResult.php">
-<h1>For you, VonVon service~<h1>
-Input your name<br >
-<input type="text" name="name" id="name" value="" >
-<br>
-Input your birthday (ex:19981105)<br>
-<input type="text" name="year" id="year" value="">
-<input type="text" name="mon" id="mon" value="">
-<input type="text" name="day" id="day" value="">
-<br>
-<input type="submit" value="View My Result">
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+?>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<?php
+$isLogin = $_COOKIE['isLogin'];
+if($isLogin != 1){
+?>
+
+<h1>log in</h1>
+<form method="post" action="postResult.php"
+ class="navbar-form navbar-left" >
+
+  <div class="form-group">
+
+    <input type="text" class="form-control" id="name" name="name"
+     placeholder="ID를 입력하세요">
+
+    <input type= "password" class="form-control" id="pw" name="pw" 
+     placeholder="암호를 입력하세요">
+    
+  </div>
+  <button type="submit" class="btn btn-default">로그인 하기</button>
 </form>
+
+<?php
+}else{
+$name = $_COOKIE['name'];
+?>
+<?=$name?>님은 이미 로그인이 되어있습니다.<br>
+로그아웃 하시려면 아래 버튼을 눌러주세요.<br>
+<a href="postLogout.php" class="btn btn-default" >log out</a>
+
+<?php
+}
+?>
--- postResult.php
+++ postResult.php
@@ -1,11 +1,40 @@
 <?php
+header("Content-Type: text/html; charset=UTF-8");
+
 $name = $_POST['name'];
-$year = $_POST['year'];
-$mon = $_POST['mon'];
-$day = $_POST['day'];
-$age = 2016 - $year +1;
+$password  = $_POST['pw'];
+
 ?>
-<h1>Wow!</h1>
-I know you, <?=$name?>!!<br>
-Your birthday is <?=$year?>-<?=$mon?>-<?=$day?><br>
-your age: <?=$age?> years old.
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<?php
+if( $name== "bts" && $password="army" ){
+   setCookie('isLogin', '1');
+   setCookie('name', $name);
+?>
+
+<h1>로그인 완료!</h1>
+I know you, your name is  <?=$name?>!!<br>
+Your password is <?=$password?><br>
+return to log in page <a href="./postForm.php">go back</a>
+
+<?php
+}else{
+setCookie('isLogin', '0');
+setCookie('name', '');
+?>
+<h1>who are you?</h1>
+no matching ID, password!<br>
+try agian.
+return to log in page <a href="./postForm.php">go back</a>
+<?php
+}
+?>
